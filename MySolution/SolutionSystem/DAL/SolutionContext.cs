d2b919a6abe3911aa746e64fecd7fb8fc2edb73b
Allan Anderson <allana@shaw.ca>
2016-11-18 15:23:28 -0700

    Added DbContext class

---
+++ MySolution/SolutionSystem/DAL/SolutionContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+//using SolutionSystem.Data.Entity;
+#endregion
+
+namespace SolutionSystem.DAL
+{
+    // This is an internal class for security reasons
+    // Access is restricted to within this class library project
+    // Inherits DbContext for Entity Framework, which requires
+    //   System.Data.Entity
+    internal class SolutionContext : DbContext
+    {
+        // Pass the connection string name to the
+        //   DbContext using :base()
+        public SolutionContext() :base("DBName")
+        {
+
+        }
+
+        // Setup all DbSet Properties once Entity classses are created
+
+    }
+}
