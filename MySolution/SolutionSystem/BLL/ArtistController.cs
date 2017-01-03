e64eba01f86384b2a9577bedfa1a7abb7f6a1e01
Allan Anderson <allana@shaw.ca>
2017-01-03 13:12:13 -0700

    More work on initial web forms

---
+++ MySolution/SolutionSystem/BLL/ArtistController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel; // for ODS
+using ChinookSystem.Data.Entities;
+using ChinookSystem.Data.POCOs;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+
+    }
+}
